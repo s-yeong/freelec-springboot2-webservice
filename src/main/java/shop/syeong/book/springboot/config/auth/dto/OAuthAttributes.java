767f9b821e56c3eab44e71edeb330eafb0d52adf
s-yeong <syxxng25@gmail.com>
2022-09-02 05:56:28 +0900

    FEAT: 네이버 로그인 연동

--- src/main/java/shop/syeong/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/shop/syeong/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -26,6 +26,11 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
+
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
     // OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환해야만 함
@@ -35,6 +40,19 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributekey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributekey(userNameAttributeName)
                 .build();
     }

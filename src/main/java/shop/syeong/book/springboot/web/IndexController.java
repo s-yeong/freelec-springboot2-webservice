444b3d817c28fabc962b5a8919d4d8f88783e47a
s-yeong <syxxng25@gmail.com>
2022-09-01 12:11:14 +0900

    FEAT: 게시글 등록 화면 구현

--- src/main/java/shop/syeong/book/springboot/web/IndexController.java
+++ src/main/java/shop/syeong/book/springboot/web/IndexController.java
@@ -5,10 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-
+// 페이지 관련된 컨트롤러
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 
     /**

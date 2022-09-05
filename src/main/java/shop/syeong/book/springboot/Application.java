ab99987e1c549162db26d150bdd6ce9908096bcd
s-yeong <syxxng25@gmail.com>
2022-09-06 01:20:55 +0900

    FEAT: 배포 자동화

--- src/main/java/shop/syeong/book/springboot/Application.java
+++ src/main/java/shop/syeong/book/springboot/Application.java
@@ -2,12 +2,16 @@ package shop.syeong.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing  // JPA Auditing 어노테이션 모두 활성화  => JpaConfig
 @SpringBootApplication  // 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정
 public class Application {  // 프로젝트의 메인 클래스
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+//        SpringApplication.run(Application.class, args);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 }
